10f4d6011420e2b4b6a2759067a7bcdba66770a2
wwwalohacampus <h850415@naver.com>
2023-11-24 15:22:14 +0900

    clear git cache

---
+++ start/start-app/src/App.js
@@ -0,0 +1,16 @@
+// React 모듈 import
+import React from 'react';
+
+// 컴포넌트 정의
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>JSX 를 사용한 React 컴포넌트 </p>
+        </div>
+    )
+}
+
+// 모듈 export
+export default App;
+
---
+++ start/start-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 컴포넌트 import
+import App from './App';
+
+
+// 메인 화면에서 App 컴포넌트 렌더링
+ReactDOM.render(<App />, document.getElementById('root'))
